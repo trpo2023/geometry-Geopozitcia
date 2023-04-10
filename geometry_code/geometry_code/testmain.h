116eb0625176cefbac7b2bc1930899e53d9a355e
Ilya Sukharenko <kailo.renn@bk.ru>
2023-04-11 00:32:17 +0700

    Add wkt-circles input/output functional

---
+++ geometry_code/geometry_code/testmain.h
@@ -0,0 +1,13 @@
+//
+//  testmain.h
+//  geometry_code
+//
+//  Created by Илья Сухаренко on 10.04.2023.
+//
+
+#ifndef testmain_h
+#define testmain_h
+
+#include <stdio.h>
+
+#endif /* testmain_h */
